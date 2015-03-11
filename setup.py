21d8e8c192fa4e25320132e8d736ce7d59b97569
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2015-03-10 21:52:21 -0300

    Improves setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,25 @@
 # encoding: utf-8
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
+__version__ = '0.1.1'
+
 setup(
     name='pagseguro-sdk',
-    version="0.1.0",
+    version=__version__,
     description='SDK para utilização do PagSeguro em Python',
-    url='http://pagseguro-sdk.readthedocs.org/',
+    url='http://github.com/jeanmask/pagseguro-sdk/',
     author='Jean O. Rodrigues',
     author_email='github@jean.bz',
-    download_url='https://github.com/jeanmask/pagseguro-sdk/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/jeanmask/pagseguro-sdk/archive/v{0}.tar.gz'.format(__version__),
     license='MIT',
-    packages=['pagseguro'],
+    packages=find_packages(exclude=('doc', 'docs', 'example')),
     install_requires=REQUIREMENTS,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -31,4 +33,5 @@ setup(
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    keywords=['pagseguro', 'sdk', 'python']
 )
